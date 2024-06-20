e05107d64fbd1e901971a708e970e680b573801e
yu seungbeom <dldkdlelqnzo@gmail.com>
2024-06-20 18:15:45 +0900

    feat: 엔티티 구현 완료

--- src/main/java/com/ktb/community/entity/comment/Comment.java
+++ src/main/java/com/ktb/community/entity/comment/Comment.java
@@ -1,13 +1,43 @@
 package com.ktb.community.entity.comment;
 
+import com.ktb.community.entity.post.Post;
+import com.ktb.community.entity.user.User;
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comments")
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+
+    @Column(name = "created_at", insertable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(insertable = false)
+    @Enumerated(EnumType.STRING)
+    private CommentStatus status;
 }
--- src/main/java/com/ktb/community/entity/user/User.java
+++ src/main/java/com/ktb/community/entity/user/User.java
@@ -1,13 +1,43 @@
 package com.ktb.community.entity.user;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(insertable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column(name = "joined_at", insertable = false)
+    private LocalDateTime joinedAt;
+
+    @Column(name = "last_login")
+    private LocalDateTime lastLogin;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
 }
